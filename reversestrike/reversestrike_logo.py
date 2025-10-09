195a30c60212eff30f91b7f9ce56366feca067c9
CodeFox <sebrinam394@gmail.com>
2025-10-09 12:51:30 -0400

    Update reversestrike_logo.py

--- reversestrike/reversestrike_logo.py
+++ reversestrike/reversestrike_logo.py
@@ -5,19 +5,17 @@ from rich.console import Console
 
 console = Console()
 
-# Create ASCII text
+
 ascii_art = pyfiglet.figlet_format("REVERSESTRIKE", font="patorjk's_cheese")
 
-# Random color list
+
 colors = ["green"]
 from termcolor import colored
 import time, os
 
 
-# Option 1: static color
 colored_art = colored(ascii_art, color=random.choice(colors))
 print(colored_art)
 
-# Option 2: rainbow gradient using rich (uncomment to use)
-# console.print(ascii_art, style="bold rainbow")
+
 
